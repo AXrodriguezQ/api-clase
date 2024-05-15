a2550a7e82d5bf6e83f1ef313863d096b37c0738
Alexander Rodriguez <160079823+AXrodriguezQ@users.noreply.github.com>
2024-05-15 20:25:44 +0200

    add config of db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,17 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [ TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: '190.147.64.47',
+    port: 5431,
+    username: 'riwi_user',
+    password: 'abcd1234',
+    database: 'books_2',
+    autoLoadEntities: true,
+    synchronize: true
+  })],
 })
+
 export class AppModule {}
